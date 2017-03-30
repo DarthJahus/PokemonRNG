28c61e0c3e8f87c15cd097e560bd99d6def2c799
DarthJahus <jahus@live.ie>
2017-03-30 01:01:52 +0100

    The code
    Mew! 👀

---
+++ PokémonRNG/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace PokémonRNG
+{
+	partial class frmGen
+	{
+		/// <summary>
+		/// Variable nécessaire au concepteur.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Nettoyage des ressources utilisées.
+		/// </summary>
+		/// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Code généré par le Concepteur Windows Form
+
+		/// <summary>
+		/// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+		/// le contenu de cette méthode avec l'éditeur de code.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.PID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.Species = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.Gender = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.Nature = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.HP = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.ATK = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.DEF = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.SPATK = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.SPDEF = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.SPEED = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.txtSeed = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.barProgress = new System.Windows.Forms.ProgressBar();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Start";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// listView1
+			// 
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.PID,
+            this.Species,
+            this.Gender,
+            this.Nature,
+            this.HP,
+            this.ATK,
+            this.DEF,
+            this.SPATK,
+            this.SPDEF,
+            this.SPEED});
+			this.listView1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.listView1.Location = new System.Drawing.Point(12, 41);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(870, 257);
+			this.listView1.TabIndex = 1;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			// 
+			// PID
+			// 
+			this.PID.Text = "PID";
+			// 
+			// Species
+			// 
+			this.Species.Text = "Species";
+			this.Species.Width = 120;
+			// 
+			// Gender
+			// 
+			this.Gender.Text = "Gender";
+			// 
+			// Nature
+			// 
+			this.Nature.Text = "Nature";
+			// 
+			// HP
+			// 
+			this.HP.Text = "HP";
+			this.HP.Width = 45;
+			// 
+			// ATK
+			// 
+			this.ATK.Text = "Atk";
+			this.ATK.Width = 45;
+			// 
+			// DEF
+			// 
+			this.DEF.Text = "Def";
+			this.DEF.Width = 45;
+			// 
+			// SPATK
+			// 
+			this.SPATK.Text = "SpAtk";
+			this.SPATK.Width = 51;
+			// 
+			// SPDEF
+			// 
+			this.SPDEF.Text = "SpDef";
+			this.SPDEF.Width = 48;
+			// 
+			// SPEED
+			// 
+			this.SPEED.Text = "Speed";
+			this.SPEED.Width = 50;
+			// 
+			// txtSeed
+			// 
+			this.txtSeed.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtSeed.Location = new System.Drawing.Point(782, 13);
+			this.txtSeed.Name = "txtSeed";
+			this.txtSeed.Size = new System.Drawing.Size(100, 23);
+			this.txtSeed.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(744, 17);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(32, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Seed";
+			// 
+			// textBox2
+			// 
+			this.textBox2.BackColor = System.Drawing.Color.Black;
+			this.textBox2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox2.ForeColor = System.Drawing.Color.PaleGreen;
+			this.textBox2.Location = new System.Drawing.Point(12, 304);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ReadOnly = true;
+			this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox2.Size = new System.Drawing.Size(870, 249);
+			this.textBox2.TabIndex = 4;
+			// 
+			// barProgress
+			// 
+			this.barProgress.Location = new System.Drawing.Point(93, 12);
+			this.barProgress.Name = "barProgress";
+			this.barProgress.Size = new System.Drawing.Size(645, 23);
+			this.barProgress.Step = 1;
+			this.barProgress.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+			this.barProgress.TabIndex = 5;
+			this.barProgress.UseWaitCursor = true;
+			// 
+			// frmGen
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(894, 565);
+			this.Controls.Add(this.barProgress);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtSeed);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.button1);
+			this.Name = "frmGen";
+			this.Text = "PokémonRNG";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.TextBox txtSeed;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.ColumnHeader PID;
+		private System.Windows.Forms.ColumnHeader Species;
+		private System.Windows.Forms.ColumnHeader Gender;
+		private System.Windows.Forms.ColumnHeader Nature;
+		private System.Windows.Forms.ColumnHeader HP;
+		private System.Windows.Forms.ColumnHeader ATK;
+		private System.Windows.Forms.ColumnHeader DEF;
+		private System.Windows.Forms.ColumnHeader SPATK;
+		private System.Windows.Forms.ColumnHeader SPDEF;
+		private System.Windows.Forms.ColumnHeader SPEED;
+		private System.Windows.Forms.ProgressBar barProgress;
+	}
+}
+
---
+++ PokémonRNG/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Jahus, 2017
+ * This work has not been finished and may never be.
+ * If you need parts of this code, feel free to copy them.
+ * 
+ * This article on Smogon Univerity
+ * describes the steps that have been coded here.
+ * http://www.smogon.com/ingame/rng/pid_iv_creation
+ * 
+ * Pushed to GitHub on 2017-03-30 @ 00:57 GMT+1
+ * 
+ */
+
+namespace PokémonRNG
+{
+	
+	public partial class frmGen : Form
+	{
+		private bool bDebug = true; // If true, shows the output to the textbox.
+		                            // Set to false if you generate more than 100 (10^2)
+		private static Random rnd = new Random();
+		public frmGen()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			// Pokémon Creation
+			// CSV file
+			using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"PocketMonsters.csv"))
+			{
+				file.WriteLine(String.Format("\"{0}\";\"{1}\";\"{2}\";\"{3}\";\"{4}\";\"{5}\";\"{6}\";\"{7}\";\"{8}\";\"{9}\";\"{10}\";\"{11}\"", "PID", "Species", "Gender", "Ability", "Nature", "HP", "Atk", "Def", "SpAtk", "SpDef", "Speed", "Seed"));
+				int max_i = (int)Math.Pow(10, 2); // Define how many Pokémon to generate,
+												  // you can go up to 100 000 (10^5) if you don't show the output (bDEBUG = false)
+				barProgress.Maximum = max_i;
+				for (int i = 0; i < max_i; i++)
+				{
+					Pokemon NewMonster = new Pokemon("");
+					decimal GenderRatio = decimal.Divide(1, 8);
+					if (bDebug) { 
+						txtSeed.Text = String.Format("0x{0:X}", NewMonster.Seed);
+						LogIt(String.Format("{0} = 0x{1:X}", "Seed", NewMonster.SeedInit));
+						LogIt(String.Format("{0} = 0x{1:X} ({1:0000000000})", "PID", (UInt32)NewMonster.PID));
+						LogIt(String.Format("Nature: {0}", NewMonster.Nature));
+						LogIt(string.Format("Gender: {0} ({1} prob.)", NewMonster.Gender, NewMonster.GenderProba));
+						LogIt(string.Format("Ability: [{0}]", NewMonster.Ability));
+						LogIt("Pokémon stats:");
+						LogIt(String.Format("\t{0}   0x{1:X} ({1})", "HP ", NewMonster.IV_HP));
+						LogIt(String.Format("\t{0}   0x{1:X} ({1})", "ATK", NewMonster.IV_ATK));
+						LogIt(String.Format("\t{0}   0x{1:X} ({1})", "DEF", NewMonster.IV_DEF));
+						LogIt(String.Format("\t{0}   0x{1:X} ({1})", "SPE", NewMonster.IV_SPEED));
+						LogIt(String.Format("\t{0}   0x{1:X} ({1})", "SPA", NewMonster.IV_SPATK));
+						LogIt(String.Format("\t{0}   0x{1:X} ({1})", "SPD", NewMonster.IV_SPDEF));
+						LogIt("");
+					}
+					file.WriteLine(String.Format("\"{0}\";\"{1}\";\"{2}\";\"{3}\";\"{4}\";\"{5}\";\"{6}\";\"{7}\";\"{8}\";\"{9}\";\"{10}\";\"{11}\"", (UInt32)NewMonster.PID, NewMonster.Species, NewMonster.Gender, NewMonster.Ability, NewMonster.Nature, NewMonster.IV_HP, NewMonster.IV_ATK, NewMonster.IV_DEF, NewMonster.IV_SPATK, NewMonster.IV_SPDEF, NewMonster.IV_SPEED, (UInt32)NewMonster.SeedInit));
+					barProgress.PerformStep();
+				}
+			}
+			
+		}
+		public void LogIt(string text)
+		{
+			// textBox2.Text += text + "\r\n"; // old way, dumb way. Don't use this, it takes too much time.
+			textBox2.AppendText(text + "\r\n");
+		}
+	}
+
+	class Pokemon
+	{
+		private static Random rnd = new Random();
+		public Boolean bDebug = false;
+		public readonly string Species;
+		public readonly Int32 PID;
+		public readonly Int32 SeedInit;
+		public readonly Int32 Seed;
+		public readonly string Nature;
+		public readonly string Gender;
+		public readonly decimal GenderProba;
+		public readonly int Ability;
+		public readonly Int16 IV_HP;
+		public readonly Int16 IV_ATK;
+		public readonly Int16 IV_DEF;
+		public readonly Int16 IV_SPATK;
+		public readonly Int16 IV_SPDEF;
+		public readonly Int16 IV_SPEED;
+		public readonly bool isInitiated;
+		protected string[] NatureConsts = new string[] { "Hardy", "Lonely", "Brave", "Adamant", "Naughty", "Bold", "Docile", "Relaxed", "Impish", "Lax", "Timid", "Hasty", "Serious", "Jolly", "Naive", "Modest", "Mild", "Quiet", "Bashful", "Rash", "Calm", "Gentle", "Sassy", "Careful", "Quirky" };
+
+		public Pokemon(string species)
+		{
+			// Seed
+			SeedInit = rnd.Next(Int32.MinValue, Int32.MaxValue);
+			Seed = SeedInit;
+			isInitiated = true;
+			// 1. Generating PID
+			Species = species;
+			Int16 PID_1 = RandomGenerator(ref Seed); Int16 PID_2 = RandomGenerator(ref Seed);
+			PID = (Int32)(PID_2 << 16) | (Int32)PID_1;
+			// 2. Nature / Gender / Ability
+			string PIDStr = String.Format("{0:0000000000}", ((UInt32)PID).ToString());
+			string NatureStr = PIDStr.Substring(PIDStr.Length - 2);
+			int NatureInt = int.Parse(NatureStr);
+			while (NatureInt > 24)
+			{
+				NatureInt -= 25;
+			}
+			Nature = NatureConsts[NatureInt];
+			// 3. Gender
+			Int32 GenderInt = PID & 0xFF;
+			string GenderStr = "Male";
+			decimal GenderRatio = decimal.Divide(1, 8); // Change GenderRatio according to species
+			if (GenderRatio == 0)
+			{
+				GenderStr = "Genderless";
+			}
+			else
+			{
+				if (GenderInt < Math.Floor(0xFF * GenderRatio))
+				{
+					GenderStr = "Female";
+					GenderRatio = 1 - GenderRatio;
+				}
+			}
+			Gender = GenderStr;
+			GenderProba = 1 - GenderRatio;
+			// 4. Ability
+			Int16 AbilityInt = (Int16)(PID & 0x01);
+			Ability = AbilityInt;
+			// 5. IVs
+			// Method 2, 3 and 4
+			// [PID] [PID] [IVs] [IVs] (Method 2)
+			// [PID] [PID] [IVs] [xxxx] [IVs] (Method 4)
+			// Use method 1 for legendaries, starters, Eevee (FRLG), Castform and Beldum (RSE)
+			// method 1 is used for all DP wild Pokémon
+			RandomGenerator(ref Seed); // Remove if method 1
+			Int16 IV_num_1 = RandomGenerator(ref Seed);
+			IV_HP = (Int16)(IV_num_1 & ((Int16)0x001F));
+			IV_ATK = (Int16)((IV_num_1 & ((Int16)0x03E0)) >> 5);
+			IV_DEF = (Int16)((IV_num_1 & ((Int16)0x7C00)) >> 10);
+			Int16 IV_num_2 = RandomGenerator(ref Seed);
+			IV_SPEED = (Int16)(IV_num_2 & ((Int16)0x001F));
+			IV_SPATK = (Int16)((IV_num_1 & ((Int16)0x03E0)) >> 5);
+			IV_SPDEF = (Int16)((IV_num_1 & ((Int16)0x7C00)) >> 10);
+		}
+
+		public Int16 RandomGenerator(ref Int32 rSeed)
+		{
+			Int64 NewSeed = Seed * 0x41C64E6D + 0x6073;
+			rSeed = (Int32)(NewSeed & 0xFFFFFFFF);
+			Int16 RandNum = (Int16)((Seed & 0xFFFF0000) >> 16);
+			return RandNum;
+		}
+	}	
+}
